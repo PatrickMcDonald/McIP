f7ffdeb0a528a6a52b3e9821a66121f035e213a5
Patrick McDonald <Patrick.McDonald@canadalife.de>
2012-10-22 11:32:58 +0100

    Initial Commit

---
+++ McIP/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace McIP
+{
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            var si = new ProcessStartInfo("ipconfig", "/all")
+            {
+                RedirectStandardOutput = true,
+                UseShellExecute = false
+            };
+
+            var p = Process.Start(si);
+
+            StreamReader reader = p.StandardOutput;
+
+            string heading = string.Empty;
+
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (!string.IsNullOrEmpty(line) && !line.StartsWith(" ", StringComparison.Ordinal))
+                {
+                    heading = line;
+                }
+
+                if (line.Contains("IP Address"))
+                {
+                    Console.WriteLine(heading);
+                    Console.WriteLine(line);
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
