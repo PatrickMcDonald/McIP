78e28682a395611cc2e045c08b491cb9c8c5fd80
Patrick McDonald <paddymcdonald@gmail.com>
2014-09-25 13:28:12 +0100

    Remove ExtensionMethods.fs, change to use active pattern

--- FsMcIP/Program.fs
+++ FsMcIP/Program.fs
@@ -1,34 +1,59 @@
 ﻿namespace FsMcIP
 
-open FsMcIP.ExtensionMethods
+open System.IO
+open System.Diagnostics
 
 module Program =
     [<EntryPoint>]
     let main argv =
-        let isHeading (line:string) = not (line.StartsWith(" "))
+        let containsAny parms (line:string) =
+            Seq.exists (fun v -> line.Contains(v)) parms
 
-        let isIPAddress (line: string) = line.ContainsAny(["IP Address"; "IPv4 Address"; "IPv6 Address"])
+        let startsWith value (line:string) =
+            line.StartsWith(value)
+
+        let readLines (reader:StreamReader) =
+            seq { while not reader.EndOfStream do
+                    yield reader.ReadLine() }
+
+        let startIPConfig() =
+            let si = new ProcessStartInfo("ipconfig", "/all", RedirectStandardOutput = true, UseShellExecute = false)
+            let p = Process.Start(si)
+            p.StandardOutput
+
+        let isHeading = startsWith " " >> not
+
+        let isIPAddress = containsAny ["IP Address"; "IPv4 Address"; "IPv6 Address"]
 
         let printIPAddress (heading, printed) line =
-            if not printed then 
-                printfn "%s" ""
+            if not printed then
+                printfn ""
                 printfn "%s" heading
             printfn "%s" line
 
-        let rec analyze (heading, printed) lines =
+        let (|Heading|IPAddress|End|Other|) lines =
             match lines with
-            | [] -> ()
-            | ""::tail -> analyze (heading, printed) tail
-            | line::tail when isHeading line -> analyze (line, false) tail
-            
-            | line::tail when isIPAddress line -> 
-                printIPAddress (heading, printed) line
-                analyze (heading, true) tail
+            | ""::tail -> Other tail
+            | line::tail when isHeading line -> Heading (line, tail)
+            | line::tail when isIPAddress line -> IPAddress (line, tail)
+            | _::tail -> Other tail
+            | [] -> End
 
-            | _::tail -> analyze (heading, printed) tail
-        
-        System.Diagnostics.Process.StartIPConfig().StandardOutput.readLines()
-            |> Seq.toList 
-            |> analyze ("", false)
+        let analyze =
+            let rec analyze' (heading, printed) = function
+                | Heading(line, tail) -> analyze' (line, false) tail
+
+                | IPAddress(line, tail) ->
+                    printIPAddress (heading, printed) line
+                    analyze' (heading, true) tail
+
+                | Other tail -> analyze' (heading, printed) tail
+                | End -> ()
+            analyze' ("", false)
+
+        startIPConfig()
+            |> readLines
+            |> List.ofSeq
+            |> analyze
 
         0 // return an integer exit code
